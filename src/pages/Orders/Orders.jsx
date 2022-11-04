6e1f3c35df21fa933ea94732e95eef85b9eaa4ad
Fulbabu <fulbabucse@gmail.com>
2022-11-04 22:46:54 +0600

    when orders is empty then show "Empty Orders"

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import { FaTimes } from "react-icons/fa";
 import toast from "react-hot-toast";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import { Link } from "react-router-dom";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
@@ -52,105 +53,125 @@ const Orders = () => {
 
   return (
     <div className="h-screen">
-      <h1 className="text-3xl font-bold text-center text-gray-700">
-        Total Orders {orders.length}
-      </h1>
-      <div className="flex flex-col">
-        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="overflow-hidden">
-              <table className="w-full lg:w-4/5 mx-auto">
-                <thead className="bg-white border-b">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6  text-left"
-                    >
-                      No
-                    </th>
-
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-center text-gray-900  py-4"
-                    >
-                      Image
-                    </th>
-
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                    >
-                      Products Name
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-center"
-                    >
-                      Products Price
-                    </th>
-
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-center"
-                    >
-                      Phone
-                    </th>
-                    <th
-                      scope="col"
-                      className="text-sm font-medium text-gray-900 px-6 py-4 text-center"
-                    >
-                      Delete
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders?.map((order, index) => (
-                    <tr
-                      key={order._id}
-                      className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
-                    >
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        {index + 1}
-                      </td>
-
-                      <td className="py-1 flex justify-center">
-                        <img
-                          src={order.image}
-                          className="w-16 h-16 rounded-lg"
-                          alt=""
-                        />
-                      </td>
-
-                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {order.serviceName}
-                      </td>
-                      <td className="text-sm text-gray-900 text-center font-light px-6 py-4 whitespace-nowrap">
-                        {order.price}
-                      </td>
-
-                      <td className="text-sm text-gray-900 text-center font-light px-6 py-4 whitespace-nowrap">
-                        {order.phone}
-                      </td>
-
-                      <td className="text-center">
-                        <button
-                          type="button"
-                          onClick={() => handleDeleteOrder(order._id)}
-                          data-mdb-ripple="true"
-                          data-mdb-ripple-color="light"
-                          className="inline-block px-4 py-2 bg-red-600  text-white font-medium text-sm leading-tight uppercase rounded shadow-lg hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
+      {orders?.length > 0 ? (
+        <div>
+          <h1 className="text-3xl font-bold text-center text-gray-700">
+            Total Orders {orders.length}
+          </h1>
+          <div className="flex flex-col">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div className="overflow-hidden">
+                  <table className="w-full lg:w-4/5 mx-auto">
+                    <thead className="bg-white border-b">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-gray-900 px-6  text-left"
                         >
-                          <FaTimes></FaTimes>
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+                          No
+                        </th>
+
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-center text-gray-900  py-4"
+                        >
+                          Image
+                        </th>
+
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                        >
+                          Products Name
+                        </th>
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-gray-900 px-6 py-4 text-center"
+                        >
+                          Products Price
+                        </th>
+
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-gray-900 px-6 py-4 text-center"
+                        >
+                          Phone
+                        </th>
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-gray-900 px-6 py-4 text-center"
+                        >
+                          Delete
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orders?.map((order, index) => (
+                        <tr
+                          key={order._id}
+                          className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+                        >
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {index + 1}
+                          </td>
+
+                          <td className="py-1 flex justify-center">
+                            <img
+                              src={order.image}
+                              className="w-16 h-16 rounded-lg"
+                              alt=""
+                            />
+                          </td>
+
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {order.serviceName}
+                          </td>
+                          <td className="text-sm text-gray-900 text-center font-light px-6 py-4 whitespace-nowrap">
+                            {order.price}
+                          </td>
+
+                          <td className="text-sm text-gray-900 text-center font-light px-6 py-4 whitespace-nowrap">
+                            {order.phone}
+                          </td>
+
+                          <td className="text-center">
+                            <button
+                              type="button"
+                              onClick={() => handleDeleteOrder(order._id)}
+                              data-mdb-ripple="true"
+                              data-mdb-ripple-color="light"
+                              className="inline-block px-4 py-2 bg-red-600  text-white font-medium text-sm leading-tight uppercase rounded shadow-lg hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
+                            >
+                              <FaTimes></FaTimes>
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col items-center space-y-5">
+          <h3 className="text-3xl font-bold text-purple-700">
+            Your Orders is Empty
+          </h3>
+          <Link to="/">
+            <button
+              type="button"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="light"
+              className="inline-block px-6 py-3 bg-purple-800 text-white font-medium text-sm leading-tight uppercase rounded shadow-lg hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+            >
+              Continue Shopping
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
